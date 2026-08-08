9a6a6c4b229c64bcdf9471b3fa8b6d1eb323a65d
mooyoung <kkmy0822@naver.com>
2026-08-09 00:45:52 +0900

    feat: 스프링부트 프로젝트 초기 설정

---
+++ src/main/java/com/suyo/suyo/SuyoApplication.java
@@ -0,0 +1,13 @@
+package com.suyo.suyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuyoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuyoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suyo/suyo/SuyoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suyo.suyo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuyoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
